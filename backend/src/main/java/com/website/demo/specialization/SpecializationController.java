375c0198c6d86195f55c35f094e95a325dac2619
Krzysztof Wyszyński <k.wyszynski@stud.elka.pw.edu.pl>
2021-04-15 11:47:13 +0200

    Specialization added

---
+++ backend/src/main/java/com/website/demo/specialization/SpecializationController.java
@@ -0,0 +1,29 @@
+package com.website.demo.specialization;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Getter
+@Setter
+@RequestMapping("specializations")
+public class SpecializationController {
+
+    private final SpecializationService specializationService;
+
+    public SpecializationController(SpecializationService specializationService) {
+        this.specializationService = specializationService;
+    }
+
+    @GetMapping
+    public List<Specialization> getAll(){
+        return specializationService.getAll();
+    }
+
+
+}
---
+++ backend/src/main/java/com/website/demo/specialization/SpecializationRepository.java
@@ -0,0 +1,8 @@
+package com.website.demo.specialization;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+}
