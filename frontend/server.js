835912e5ccd74e8cb293b407dd80a78286b5e198
Krzysztof Wyszyński <wukrzychu@gmail.com>
2021-11-09 22:56:40 +0100

    connect-history added for vue router

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback')
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
