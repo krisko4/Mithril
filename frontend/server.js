3e498b4b21b20337d7c156ba74068d0a0a85357a
Krzysztof Wyszyński <wukrzychu@gmail.com>
2021-11-09 20:50:10 +0100

    cors configuration

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors')
+var corsOptions = {
+  origin: `${process.env.VUE_APP_BASE_URL}`,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
@@ -8,4 +12,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('hello world!')
 });
-app.use(cors)
+app.use(cors(corsOptions))
